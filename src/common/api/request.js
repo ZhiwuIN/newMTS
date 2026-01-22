f3ea9bbdb5a92d5bd7e8f078286a9c8a913a6a1e
xiaolongstart <xiaolongstart@163.com>
2026-01-23 00:18:12 +0800

    修改为cli项目以支持ci构建

--- src/common/api/request.js
+++ src/common/api/request.js
@@ -21,12 +21,7 @@ const {
 // 防止请求失败时重复弹窗
 let globalRequestFailedAlerted = false;
 
-// 全局请求封装——也可像上述一样根据环境不同的判断
-// const base_url = '/api';8080'; //开发环境
-// const base_url = 'http://192.168.0.9:8081' // 开发环境
-// const base_url = 'http://18.163.208.5:8881' // 测试环境
-const base_url = 'https://api.cwpc.cc' // 域名
-// const base_url = 'https://api.itslai.com' // 域名
+const base_url = import.meta.env.VITE_API_URL ?? 'https://api.cwpc.cc';
 
 // 请求超时设置
 const timeout = 30000;
